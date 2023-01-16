d20a7ecf185818cf5dfaa00b2822152d3597e279
AnaaQA <ana.todos.qa@gmail.com>
2023-01-16 18:58:38 -0300

    Jest

---
+++ calculadora.spec.js
@@ -0,0 +1,26 @@
+const calculo = require('./soma')
+const sub = require('./sub')
+const mult = require('./mult')
+const div = require('./div')
+
+test ('div dois Numeros', () =>{
+
+    expect (div.div(26,2)).toBe(13)
+
+})
+test ('soma dois Numeros', () =>{
+
+    expect (calculo.soma(10,20)).toBe(30)
+
+})
+test ('sub um Numeros', () =>{
+
+    expect (sub.subtracao(20,10)).toBe(10)
+    
+})
+
+test ('mult um  vezes Numeros', () =>{
+
+    expect (mult.mult(5,3)).toBe(15)
+    
+})
---
+++ div.js
@@ -0,0 +1,6 @@
+function div (num1, num2)
+{
+    return num1/num2;
+}
+
+exports.div=div
